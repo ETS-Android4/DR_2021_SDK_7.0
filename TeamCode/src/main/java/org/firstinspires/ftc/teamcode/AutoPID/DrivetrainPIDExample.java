2e8f58f9e78a526e9ff1804036b57bf02d5effed
LauraAEvans <elzagirl10@gmail.com>
2021-11-27 11:35:52 -0600

    - added pid control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPID/DrivetrainPIDExample.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.AutoPID;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class DrivetrainPIDExample extends LinearOpMode {
+    DcMotorEx left;
+    DcMotorEx right;
+
+    // target in encoder ticks for each motor
+    double targetLeft = 1000;
+    double targetRight = 1000;
+
+
+    // initialize error
+    double leftError = 1000;
+    double rightError = 1000;
+
+    // proportional gain
+    double Kp = 0.1;
+
+    // pid controllers set with arbitrary values, use at your own risk
+    PIDController leftController = new PIDController(0.5,0.1,0.2);
+    PIDController rightController = new PIDController(0.5,0.1,0.2);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        // configure your motors and other hardware stuff here
+        // make sure the strings match the names that you have set on your robot controller configuration
+        left = hardwareMap.get(DcMotorEx.class,"left");
+        right = hardwareMap.get(DcMotorEx.class,"right");
+
+
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // reverse the appropriate motor
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // wait until the start button is pressed
+        waitForStart();
+
+
+        /*
+         * use proportional feedback to move drive train to reference
+         */
+        while (opModeIsActive()) {
+
+            left.setPower(leftController.output(targetLeft,left.getCurrentPosition()));
+            right.setPower(rightController.output(targetRight,right.getCurrentPosition()));
+
+        }
+
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPID/readme.md
@@ -0,0 +1,2 @@
+## Code Examples!
+These code examples are aimed to be a guide to implement your own controllers in FTC.  It is likely that these examples and especially the PID examples will need tuning to get working on your hardware.  
